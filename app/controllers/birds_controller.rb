b93aacd925ddf8de2be91970237098a3701c2615
Sabiha Bary <sabihaab9@gmail.com>
2022-07-31 18:44:26 -0400

    commit tree e9c2d388d7fc8a7a7501010f98aa5ea6d7e2e096

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,7 +8,9 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params) 
+    # Bird.create(name: params[:name], species: params[:species])
+    
     render json: bird, status: :created
   end
 
@@ -22,4 +24,10 @@ class BirdsController < ApplicationController
     end
   end
 
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
 end
